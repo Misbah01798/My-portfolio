ea192f93255722797efd18b737a8dd03b686194b
Misbah01798 <misbahuddin01798@gmail.com>
2023-12-12 18:31:53 +0600

    firebase seeting

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ01ebAAU4l6a1KZSiwHOWvafKBDAi7JQ",
+  authDomain: "next-my-portfolio.firebaseapp.com",
+  projectId: "next-my-portfolio",
+  storageBucket: "next-my-portfolio.appspot.com",
+  messagingSenderId: "326079689638",
+  appId: "1:326079689638:web:78ac130d1036c816d11567"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
